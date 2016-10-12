47e4920a4623800d7f3acd81bafe251fe868be1b
tj000b <tj000b@gmail.com>
2016-10-12 14:22:38 +0200

    ^

--- PIR.py
+++ PIR.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO		#include the RPi.GPIO module.
 import time
 
 pirPin = 7
+pirState = 0
 
 GPIO.setmode(GPIO.BOARD)	#Use pi board pin numbering
 GPIO.setup(pirPin, GPIO.IN)		#Set pin7 as input
@@ -10,11 +11,15 @@ GPIO.setup(pirPin, GPIO.IN)		#Set pin7 as input
 print("Here we go - CTRL+C to exit")
 try:
 	while 1:
-		if GPIO.input(pirPin):
-			print("motion detected")
-			time.sleep(0.1)
+		pirRead = GPIO.input(pirPin)		#read input value
+		if pirRead:							#if value is true
+			if pirState == 0				#we only want notification if the motion has just started
+				print("motion detected")	
+				pirState = 1				#set variable to note that motions has begun
 		else:
-			print("no activity")
-			time.sleep(0.1)
+			if pirState == 1				#if motion has begun --> 
+				print("motion ended")		#and we no longe detect motion. Print motion ended
+				pirState = 0
+			#time.sleep(0.1)
 except KeyboardInterrupt:	#If CTRL+C is pressed, exit cleanly
 	GPIO.cleanup() 				#Garbage collection
