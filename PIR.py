fb2584df21d881e5a6bc0c233f1f550d3c88cd0f
tj000b <tj000b@gmail.com>
2016-10-12 13:23:04 +0200

    input

--- PIR.py
+++ PIR.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO		#include the RPi.GPIO module.
 
 GPIO.setmode(GPIO.BOARD)	#Use pi board pin numbering
 
-GPIO.setup(7.GPIO.IN)		#Set pin7 as input
+GPIO.setup(7, GPIO.IN)		#Set pin7 as input
 
 if GPIO.input(7):
 	print("motion detected")
